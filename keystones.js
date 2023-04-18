91d1df08d9fb1c361c6956da86bb56fca725ac6b
Wossaname <66016010+Wossaname@users.noreply.github.com>
2023-04-18 13:22:16 +0800

    Copy of existing tool

---
+++ keystones.js
@@ -0,0 +1,230 @@
+var keystones = [		
+	{
+		"id": 1,
+		"name": "Ancestral Bond", 
+		"description": "Can have up to 1 additional Totem summoned at a time\nYou can't deal Damage with your Skills yourself"
+	},
+	
+	{
+		"id": 2,
+		"name": "Avatar of Fire",
+	    "description": "50% of Physical, Cold and Lightning Damage Converted to Fire Damage\nDeal no Non-Fire Damage"
+	},
+	
+	{
+		"id": 3,
+		"name": "Blood Magic", 
+		"description": "Removes all mana.\nSpend Life instead of Mana for Skills"
+	},
+	
+	{
+		"id": 4,
+		"name": "Chaos Inoculation", 
+		"description": "Maximum Life becomes 1\nImmune to Chaos Damage"
+	},
+	
+	{
+		"id": 5,
+		"name": "Conduit", 
+		"description": "Share Endurance, Frenzy and Power Charges with nearby party members"
+	},
+	
+	{
+		"id": 6,
+		"name": "Eldritch Battery", 
+		"description": "Energy Shield protects Mana instead of Life\nSpend Energy Shield before Mana for Skill Costs"
+	},
+	
+	{
+		"id": 7,
+		"name": "Elemental Equilibrium", 
+		"description": "Hits that deal Elemental Damage remove Exposure to those Elements and inflict Exposure to other Elements.\nExposure inflicted this way applies -25% to Resistances."
+	},
+	
+	{
+		"id": 8,
+		"name": "Ghost Reaver", 
+		"description": "Life Leech is applied to Energy Shield instead\nMaximum total Energy Shield Recovery per second from Leech is Doubled.\nCannot Recharge Energy Shield"
+	},
+	
+	{
+		"id": 9,
+		"name": "Iron Grip", 
+		"description": "Strength's Damage bonus applies to Projectile Attack Damage as well as Melee Damage"
+	},
+	
+	{
+		"id": 10,
+		"name": "Iron Reflexes", 
+		"description": "Convert all Evasion Rating to Armour.\nDexterity provides no bonus to Evasion Rating"
+	},
+	
+	{
+		"id": 11,
+		"name": "Mind Over Matter",
+		 "description": "30% of Damage is taken from Mana before Life"
+	},
+	
+	{
+		"id": 12,
+		"name": "Minion Instability",
+		"description": "Minions explode when reduced to low life, dealing 33% of their maximum life as fire damage to surrounding enemies"
+	},
+	
+	{
+		"id": 13,
+		"name": "Necromantic Aegis", 
+		"description": "All bonuses from an equipped Shield apply to your Minions instead of you"
+	},
+	
+	{
+		"id": 14,
+		"name": "Pain Attunement", 
+		"description": "30% more Spell Damage when on Low Life"
+	},
+	
+	{
+		"id": 15,
+		"name": "Acrobatics", 
+		"description": "Modifiers to Chance to Suppress Spell Damage instead apply to Chance to Dodge Spell hits at 50% of their Value.\n Maximum chance to Dodge Spell Hits is 75%."
+	},
+	
+	{
+		"id": 16,
+		"name": "Point Blank", 
+		"description": "Projectile Attacks deal up to 30% more Damage to targets at the start of their movement, dealing less Damage to targets as the projectile travels farther"
+	},
+	
+	{
+		"id": 17,
+		"name": "Resolute Technique", 
+		"description": "Your hits can't be Evaded\nNever deal Critical Strikes"
+	},
+	
+	{
+		"id": 18,
+		"name": "Unwavering Stance", 
+		"description": "Cannot Evade enemy Attacks\nCannot be Stunned"
+	},
+	
+	{
+		"id": 19,
+		"name": "Vaal Pact", 
+		"description": "Total Recovery per second from Life Leech is doubled\nMaximum total Life Recovery per second from Leech is doubled\nLife Regeneration has no effect"
+	},
+	
+	{
+		"id": 20,
+		"name": "Zealot's Oath", 
+		"description": "Life Regeneration applies to Energy Shield instead of Life"
+	},
+
+	{
+		"id": 21,
+		"name": "Arrow Dancing",
+		"description": "40% more chance to Evade Projectile Attacks\n20% less chance to Evade Melee Attacks"
+	},
+
+	{
+		"id": 22,
+		"name": "Crimson Dance",
+		"description": "You can inflict Bleeding on an Enemy up to 8 times\nYour Bleeding does not deal extra Damage while the Enemy is moving\n50% less Damage with Bleeding"
+	},
+
+	{
+		"id": 23,
+		"name": "Elemental Overload",
+		"description": "Skills that have dealt a Critical Strike in the past 8 seconds deal 40% more Elemental Damage with Hits and Ailments\nYour Critical Strikes do not deal extra Damage\nAilments never count as being from Critical Strikes"
+	},
+
+	{
+		"id": 24,
+		"name": "Perfect Agony",
+		"description": "Modifiers to Critical Strike Multiplier also apply to Damage Multiplier for Ailments from Critical Strike at 50% of their value\n30% less Damage with Hits"
+	},
+
+	{
+		"id": 25,
+		"name": "Glancing Blows",
+		"description": "Chance to Block Attack Damage is doubled\nChance to Block Spell Damage is doubled\nYou take 65% of Damage from Blocked Hits"
+	},
+
+	{
+		"id": 26,
+		"name": "Call to Arms",
+		"description": "Using Warcries is Instant\nWarcries share their Cooldown"
+	},
+
+	{
+		"id": 27,
+		"name": "Agnostic",
+		"description": "Maximum Energy shield is 0.\nWhile not on full life, sacrifice 20% of mana per second to recover that much life."
+	},	
+
+	{
+		"id": 28,
+		"name": "Iron Will",
+		"description": "Strength's Damage bonus applies to all Spell Damage as well."
+	},	
+
+	{
+		"id": 29,
+		"name": "Eternal Youth",
+		"description": "50% less Life Regeneration Rate.\n50% less maximum Total Recovery per Second from Life Leech.\nEnergy Shield Recharge instead applies to Life."
+	},	
+	{
+		"id": 30,
+		"name": "Imbalanced Guard",
+		"description": "100% chance to Defend with Double Armour\nMaximum Damage Reduction for any Damage Type is 50%."
+	},	
+	{
+		"id": 31,
+		"name": "Divine Shield",
+		"description": "Cannot Recover Energy Shield to above Armour.\n3% of Total Physical Damage prevented from Hits Recently is Regenerated as Energy Shield per second."
+	},	
+	{
+		"id": 32,
+		"name": "Versatile Combatant",
+		"description": "-25% to maximum Chance to Block Attack Damage\n.-25% to maximum Chance to Block Spell Damage\n.+2% Chance to Block Spell Damage for each 1% Overcapped Chance to Block Attack Damage"
+	},	
+	{
+		"id": 33,
+		"name": "Wind Dancer",
+		"description": "20% less Attack Damage taken if you haven't been Hit by an Attack Recently\n40% more Evasion Rating if you have been Hit by an Attack Recently\n20% more Attack Damage taken if you have been Hit by an Attack Recently"
+	},	
+	{
+		"id": 34,
+		"name": "Magebane",
+		"description": "Dexterity provides no inherent bonus to Evasion Rating\n+1% Chance to Suppress Spell Damage per 15 Dexterity"
+	},	
+	{
+		"id": 35,
+		"name": "Supreme Ego",
+		"description": "You can only have one Permanent Aura on you from your Skills\nAuras from your Skills do not affect Allies\nAuras from your Skills have 50% more Effect on you\n50% more Mana Reserved"
+	},	
+	{
+		"id": 36,
+		"name": "Solipsism",
+		"description": "Intelligence provides no inherent bonus to Energy Shield\n2% reduced Duration of Elemental Ailments on you per 15 Intelligence"
+	},	
+	{
+		"id": 37,
+		"name": "Doomsday",
+		"description": "Your Hex Curse Spells create a Hexed Area for 1 second if you Cast them yourself\nEnemies in the Hexed Area are Hexed, and the Hex does not expire while in the Hexed Area\nHexes applied this way do not gain Doom over time\nHexes in the Hexed Area gain up to maximum Doom when the Hexed Area ends"
+	},	
+	{
+		"id": 38,
+		"name": "Wicked Ward",
+		"description": "Energy Shield Recharge is not interrupted by Damage if Recharge began Recently\n50% less Energy Shield Recharge Rate\n"
+	},	
+	{
+		"id": 39,
+		"name": "Lethe Shade",
+		"description": "Take 50% less Damage over Time if you've started taking Damage over Time in the past second\n100% more Duration of Ailments on you"
+	},	
+	{
+		"id": 40,
+		"name": "Ghost Dance",
+		"description": "Cannot Recover Energy Shield to above Evasion Rating\nEvery 2 seconds, gain a Ghost Shroud, up to a maximum of 3\nWhen Hit, lose a Ghost Shroud to Recover Energy Shield equal to 3% of your Evasion Rating"
+	},	
+];
